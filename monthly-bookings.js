f47f8b65bbb96cd65b9bebc597796dfed4c7868c
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-02 15:17:43 +0700

    Create monthly-bookings.js

---
+++ monthly-bookings.js
@@ -0,0 +1,8 @@
+let bookingDays = 20
+let output = ''
+if (bookingDays < 30) {
+  output = 'Your booking is being processed'
+} else {
+    output = 'Please contact us for monthly bookings'
+}
+return output
