1fd9ca2c9e0c38ad0beb09149037db61c9e11f41
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-07 12:16:17 +0700

    Create counter method for counter

--- objects-b.js
+++ objects-b.js
@@ -173,3 +173,54 @@ function removePasswords(users) {
 
 console.log(removePasswords([{'name':'Jack','age':45},{'name':'Bob','age':27,'password':'9!4%2!@'}]))
 
+// Create a Method
+
+// Create an object greeter and add a method sayHello to it.
+// The method should return 'Hello World'
+
+let greeter1 = {
+    sayHello: function() {
+        return 'Hello World'
+    }
+}
+
+console.log(greeter1.sayHello()) // 'Hello World'
+
+// Greet by name
+
+// Create an object greeter.
+// In the object, create a method sayHello that takes a parameter, 
+// then returns a string as per the tests below.
+
+let greeter = {
+    sayHello: function(name) {
+        return 'Hello ' + name
+    }
+}
+
+console.log(greeter.sayHello('John')) // 'Hello John'
+greeter.sayHello('Alice') // 'Hello Alice'
+greeter.sayHello() // 'Hello guest'
+
+
+// Counter
+
+// Create an object counter with 2 methods: increase and decrease.
+// Each method takes an object, as per the below tests, and returns the object 
+// with internal count value either increased by 1 or decreased by 1.
+
+let counter = {
+    count: 0,
+    increase: function(counter) {
+        counter.count++
+        return counter
+    },
+    decrease: function(counter) {
+        counter.count--
+        return counter
+    }
+}
+console.log(counter.increase({ count: 1 })) // { count: 2 }
+console.log(counter.increase({ count: 10 })) // { count: 11 }
+console.log(counter.decrease({ count: 1 })) // { count: 0 }
+console.log(counter.decrease({ count: 10 })) // { count: 9 }
