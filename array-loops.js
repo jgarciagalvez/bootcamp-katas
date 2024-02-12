7ec9eed5010b51a8b1ae6f74f22fe85afe5237a2
Jesus <40635808+jgarciagalvez@users.noreply.github.com>
2024-02-12 11:52:53 +0700

    Solve Filter Katas

--- array-loops.js
+++ array-loops.js
@@ -76,3 +76,95 @@ FILTER KATAS
 ################################################
 */
 
+// Refactoring Filter
+// You are given a function that takes an array of numbers and returns a new array containing only even numbers.
+// Your task is to refactor this function to use the filter() method instead.
+
+// function getEvenNumbers(numbers) {
+//     const evenNumbers = [];
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] % 2 === 0) {
+//             evenNumbers.push(numbers[i]);
+//         }
+//     }
+//     return evenNumbers;
+// }
+// function getEvenNumbers(numbers) {
+//     const evenNumbers = numbers.filter(n => n % 2 === 0)
+//     return evenNumbers
+// }
+
+const getEvenNumbers = numbers => numbers.filter(n => n % 2 === 0)
+
+// Tests
+
+console.log(getEvenNumbers([1,2,3,4,5,6])) // [2,4,6]
+console.log(getEvenNumbers([0,-10,11,-12,13])) // [0,-10,-12]
+console.log(getEvenNumbers([])) // []
+
+
+
+// Filter Negatives
+
+// Write a JavaScript function named filterOutNegativeNumbers.
+// This function should take an array of numbers and return a new array with all negative numbers filtered out, leaving only positive numbers and zeros.
+
+function filterOutNegativeNumbers1(numbers) {
+    let negativeNumbers = numbers.filter(n => n >= 0)
+    return negativeNumbers
+}
+
+const filterOutNegativeNumbers = numbers => numbers.filter(n => n >= 0)
+
+console.log(filterOutNegativeNumbers([1,2,3])) // [1,2,3]
+console.log(filterOutNegativeNumbers([1,-2,-3,4])) // [1,4]
+console.log(filterOutNegativeNumbers([-10,-20,-30,-40])) // []
+
+
+// Long Strings Bug
+
+// You are given a function that is intended to return an array of strings that are longer than 4 characters.
+// However, there's a mistake in the implementation of the filter() method.
+// Your task is to identify and correct the error in the function.
+
+// function getLongStrings(strings) {
+//     return strings.filter(string => {
+//         if (string.length > 4)
+//     })
+// }
+
+function getLongStrings1(strings) {
+    return strings.filter(string =>
+        string.length > 4
+    )
+}
+
+const getLongStrings = strings => strings.filter(str => str.length > 4)
+
+console.log(getLongStrings(['Hello','Hola','Ciao'])) // ['Hello']
+console.log(getLongStrings(['Grazie','Merci','Tak'])) // ['Grazie','Merci']
+console.log(getLongStrings(['Grazie','Merci','Tak','Kop Khun'])) // ['Grazie','Merci','Kop Khun']
+
+
+
+// Filter and Square Evens
+
+// Create a JavaScript function named filterAndSquareEvens. This function should take an array of numbers as its input. It should first filter out all odd numbers, and then square each of the remaining even numbers. The function should return a new array with these squared even numbers.
+
+const filterAndSquareEvens1 = numbers => {
+    let evenNumbers = numbers.filter(n => n % 2 === 0)
+    let sqrEvenN= evenNumbers.map(n => n * n)
+    return sqrEvenN
+}
+
+// Simplified function
+
+const filterAndSquareEvens = numbers => numbers.filter(n => n % 2 === 0).map(n => n * n)
+
+// Tests
+
+console.log(filterAndSquareEvens([1,2,3,4,5,6])) // [4,16,36]
+console.log(filterAndSquareEvens([10,-10,20,-20])) // [100,100,400,400]
+console.log(filterAndSquareEvens([0,11,22,0])) // [0,484,0]
+
+
